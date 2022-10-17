9a7bb116c64f445adfa315a1394570431fe60d23
Helder Paiva <helder.devjava@gmail.com>
2022-10-16 23:52:23 -0300

    segundo commit - implementando o ProductController

---
+++ laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /* Vou criar o controlador conforme o tutorial abaixo
+    https://techvblogs.com/blog/build-crud-app-with-laravel-and-reactjs
+    depois vou pegar o ControllerTrait (é uma classe onde tem códigos reusaveis)
+    neste caso os metodos __construct que recebe a instancia, e os demais:
+    index, indexAll, store, show, update, 
+    */
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ProductDB::select('id','tile','description','image')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image'=>'required|image'
+        ]);
+
+        try{
+            $imageName = Str::random().'.'.$request->image->getClientOriginalExtension();
+            Storage::disk('public')->putFileAs('product/image', $request->image,$imageName);
+            Product::create($request->post()+['image'=>$imageName]);
+
+            return response()->json([
+                'message'=>'Product Created Sussessfully!!'
+            ]);
+        }catch(\Exception $e){
+            \Log::error($e->getMenssage());
+            return response()->json([
+                'message'=>'Somethig goes wrong while creating a product!!'
+            ],500);
+
+        }
+        
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return response()->json([
+            'product'=>$product
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request, Product $product)
+    {
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'image'=>'nullable'
+        ]);
+
+        try{
+
+            $product->fill($request->post())->update();
+
+            if($request->hasFile('image')){
+
+                // remove old image
+                if($product->image){
+                    $exists = Storage::disk('public')->exists("product/image/{$product->image}");
+                    if($exists){
+                        Storage::disk('public')->delete("product/image/{$product->image}");
+                    }
+                }
+
+                $imageName = Str::random().'.'.$request->image->getClientOriginalExtension();
+                Storage::disk('public')->putFileAs('product/image', $request->image,$imageName);
+                $product->image = $imageName;
+                $product->save();
+            }
+
+            return response()->json([
+                'message'=>'Product Updated Successfully!!'
+            ]);
+
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'Something goes wrong while updating a product!!'
+            ],500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        try {
+
+            if($product->image){
+                $exists = Storage::disk('public')->exists("product/image/{$product->image}");
+                if($exists){
+                    Storage::disk('public')->delete("product/image/{$product->image}");
+                }
+            }
+
+            $product->delete();
+
+            return response()->json([
+                'message'=>'Product Deleted Successfully!!'
+            ]);
+            
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'message'=>'Something goes wrong while deleting a product!!'
+            ]);
+        }
+    }
+}
